f7b1afe1eea12157af92165729bdfc4cc655578b
Hexadecane <36948730+Hxdce@users.noreply.github.com>
2023-02-01 04:53:06 -0500

    Add camera component & smooth crouching to player

--- Source/ProjectHoplomancer/PlayerCharacter/PlayerCharacter.cpp
+++ Source/ProjectHoplomancer/PlayerCharacter/PlayerCharacter.cpp
@@ -7,16 +7,29 @@
 #include "EnhancedInputSubsystems.h"
 
 
-// Sets default values
+// Sets default values.
 APlayerCharacter::APlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a first person camera component.
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	check(PlayerCamera != nullptr);
+	// Attach the camera component to our root component.
+	PlayerCamera->SetupAttachment(GetRootComponent());
+	// Position the camera slightly above the eyes.
+	PlayerCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight));
+	// Enable the pawn to control camera rotation.
+	PlayerCamera->bUsePawnControlRotation = true;
+
+	// Used for smooth crouching.
+	CrouchEyeOffset = FVector(0.0f);
+	CrouchSpeed = 12.0f;
 }
 
 
-// Called when the game starts or when spawned
+// Called when the game starts or when spawned.
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
@@ -36,15 +49,7 @@ void APlayerCharacter::BeginPlay()
 }
 
 
-// Called every frame
-void APlayerCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-
-// Called to bind functionality to input
+// Called to bind functionality to input.
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -54,12 +59,14 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
+		EnhancedInputComponent->BindAction(CrouchStartAction, ETriggerEvent::Triggered, this, &APlayerCharacter::StartCrouch);
+		EnhancedInputComponent->BindAction(CrouchStopAction, ETriggerEvent::Triggered, this, &APlayerCharacter::StopCrouch);
 	}
 
 }
 
 
-// The main player movement function
+// The main player movement function.
 void APlayerCharacter::Move(const FInputActionValue& Value)
 {	
 	// Directional character movement. Reacts according to the camera's rotation.
@@ -75,11 +82,76 @@ void APlayerCharacter::Move(const FInputActionValue& Value)
 }
 
 
-// The main player looking function
+// The main player looking function.
 void APlayerCharacter::Look(const FInputActionValue& Value)
 {
 	const FVector2D LookAxisVector = Value.Get<FVector2D>();
 
 	AddControllerPitchInput(LookAxisVector.Y);
 	AddControllerYawInput(LookAxisVector.X);
+}
+
+
+// Start crouching...
+void APlayerCharacter::StartCrouch(const FInputActionValue& Value)
+{
+	Crouch();
+}
+
+
+// Stop crouching...
+void APlayerCharacter::StopCrouch(const FInputActionValue& Value)
+{
+	UnCrouch();
+}
+
+
+// OnStartCrouch override.
+void APlayerCharacter::OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust)
+{
+	if (HalfHeightAdjust == 0.0f)
+	{
+		return;
+	}
+
+	float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnStartCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight + HalfHeightAdjust;
+	PlayerCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight), false);
+}
+
+
+// OnEndCrouch override.
+void APlayerCharacter::OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust)
+{
+	if (HalfHeightAdjust == 0.0f)
+	{
+		return;
+	}
+
+	float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnEndCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight - HalfHeightAdjust;
+	PlayerCamera->SetRelativeLocation(FVector(0.0f, 0.0f, BaseEyeHeight), false);
+}
+
+
+// CalcCamera override.
+void APlayerCharacter::CalcCamera(float DeltaTime, struct FMinimalViewInfo& OutResult)
+{
+	if (PlayerCamera)
+	{
+		PlayerCamera->GetCameraView(DeltaTime, OutResult);
+		OutResult.Location += CrouchEyeOffset;
+	}
+}
+
+
+// Called every frame.
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float CrouchInterpolationTime = FMath::Min(1.0f, CrouchSpeed * DeltaTime);
+	CrouchEyeOffset = (1.0f - CrouchInterpolationTime) * CrouchEyeOffset;
 }
--- Source/ProjectHoplomancer/PlayerCharacter/PlayerCharacter.h
+++ Source/ProjectHoplomancer/PlayerCharacter/PlayerCharacter.h
@@ -6,6 +6,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
 // This include stays at the bottom:
 #include "PlayerCharacter.generated.h"
@@ -25,24 +27,48 @@ public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
+	void OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+
+	void OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+
+	void CalcCamera(float DeltaTime, struct FMinimalViewInfo& OutResult) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=Camera)
+	UCameraComponent* PlayerCamera;
+
 	UPROPERTY(EditAnywhere, Category=Input)
 	UInputMappingContext* PlayerCharacterContext;
 
-	UPROPERTY(EditAnywhere, Category = Input)
+	UPROPERTY(EditAnywhere, Category=Input)
 	UInputAction* MovementAction;
 
-	UPROPERTY(EditAnywhere, Category = Input)
+	UPROPERTY(EditAnywhere, Category=Input)
 	UInputAction* LookAction;
 
-	UPROPERTY(EditAnywhere, Category = Input)
+	UPROPERTY(EditAnywhere, Category=Input)
 	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* CrouchStartAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* CrouchStopAction;
+
+
+	// Used for smooth crouching.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Crouch)
+	FVector CrouchEyeOffset;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Crouch)
+	float CrouchSpeed;
 
 	void Move(const FInputActionValue& Value);
 	void Look(const FInputActionValue& Value);
+	void StartCrouch(const FInputActionValue& Value);
+	void StopCrouch(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
