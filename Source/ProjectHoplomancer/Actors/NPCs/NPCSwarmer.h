da594d4df402bb0c42c28c0c43868e7111c1d6dc
Hxdce <36948730+Hxdce@users.noreply.github.com>
2023-04-07 16:25:16 -0400

    Add NPCSwarmer + basic AI behavior

---
+++ Source/ProjectHoplomancer/Actors/NPCs/NPCSwarmer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../BaseNPC.h"
+#include "NPCSwarmer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHOPLOMANCER_API ANPCSwarmer : public ABaseNPC
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this character's properties
+	ANPCSwarmer();
+};
