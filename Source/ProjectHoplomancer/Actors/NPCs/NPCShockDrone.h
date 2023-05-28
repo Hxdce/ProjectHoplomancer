14a02c5cd80c55ffbf84e692199154b4c2770e8a
Hxdce <36948730+Hxdce@users.noreply.github.com>
2023-05-27 23:39:17 -0400

    Add NPC Shock Drone boilerplate

---
+++ Source/ProjectHoplomancer/Actors/NPCs/NPCShockDrone.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../BaseNPC.h"
+#include "NPCShockDrone.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHOPLOMANCER_API ANPCShockDrone : public ABaseNPC
+{
+	GENERATED_BODY()
+	
+};
