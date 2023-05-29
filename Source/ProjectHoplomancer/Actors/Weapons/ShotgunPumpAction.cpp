6460d68311d25fe0f8c7b27fa8881bd6b109d6a4
Hxdce <36948730+Hxdce@users.noreply.github.com>
2023-05-29 11:09:50 -0400

    Implement pump-action shotgun firing & ammo type

--- Source/ProjectHoplomancer/Actors/Weapons/ShotgunPumpAction.cpp
+++ Source/ProjectHoplomancer/Actors/Weapons/ShotgunPumpAction.cpp
@@ -13,7 +13,7 @@ AShotgunPumpAction::AShotgunPumpAction()
 
 	// Default values for weapon stats:
 	PrintName = "Pump-Action Shotgun";
-	DamagePrimary = 6.0f;
+	DamagePrimary = 7.0f;
 	ProjectileCount = 8;
 	Firerate = 1.5f;
 	ProjectileVelocity = 10000.0f;
@@ -48,7 +48,85 @@ void AShotgunPumpAction::Tick(float DeltaTime)
 // Primary attack!
 void AShotgunPumpAction::PrimaryAttack(AActor* Parent, FVector MuzzleLocation, FRotator MuzzleRotation)
 {
+	UWorld* World = GetWorld();
+	// Basic check for valid world + can't fire faster than weapon firerate + can't fire while reloading.
+	if (!Parent || !World || World->GetTimeSeconds() < NextFireTime || IsReloading)
+	{
+		return;
+	}
+	if (ReservoirCurrRoundCount <= 0)
+	{
+		// Should dry fire here, then reload if we have ammo.
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Weapon is empty! Click!"));
+		NextFireTime = GetWorld()->GetTimeSeconds() + 0.5f;
+		return;
+	}
 
+	if (WeaponProjectile)
+	{
+		FActorSpawnParameters SP;
+		SP.Owner = Parent;
+		if (SP.Owner != nullptr)
+		{
+			SP.Instigator = SP.Owner->GetInstigator();
+		}
+
+		bool fired = false;
+
+		// Create and fire off the shotgun pellets.
+		for (int i = 0; i < ProjectileCount; i++)
+		{
+			ABaseProjectile* Projectile = World->SpawnActor<ABaseProjectile>(WeaponProjectile, MuzzleLocation, MuzzleRotation, SP);
+			if (Projectile)
+			{
+				fired = true;
+				// Set up projectile properties here? (Might be better to make this its own separate function, or distinct ammo type classes!)
+				Projectile->ProjectileFirer = SP.Owner->GetInstigatorController();
+				Projectile->MovementComponent->InitialSpeed = ProjectileVelocity;
+				Projectile->MovementComponent->MaxSpeed = ProjectileVelocity;
+				Projectile->DamageValue = DamagePrimary;
+
+				// Set the projectile's initial rotation.
+				FRotator CurrentLaunchDirection = MuzzleRotation;
+
+				// Randomize projectile directions for shotgun spread.
+				// The projectile spread value from the player's POV represents the diameter of a circle in degrees.
+				// Create a random polar coordinate within half of the max spread (that circle's radius), then
+				// convert it to cartesian coordinates for the random pitch and yaw change.
+				double r = FMath::RandRange(0.0, 3.0);
+				double theta = FMath::RandRange(0.0, 360.0);
+				double x = r * FMath::Cos(theta);
+				double y = r * FMath::Sin(theta);
+				CurrentLaunchDirection.Pitch += x;
+				CurrentLaunchDirection.Yaw += y;
+
+				// Randomize projectile directions for shotgun spread.
+				Projectile->FireInDirection(CurrentLaunchDirection.Vector());
+
+				//FVector traceStart = MuzzleLocation;
+				//FVector traceEnd = MuzzleLocation + MuzzleRotation.Vector() * 100000.0f;
+
+				//DrawDebugLine(GetWorld(), traceStart, traceEnd, FColor::Red, false, 5.0f, 0, 2.0f);
+			}
+		}
+
+		if (fired)
+		{
+			ApplyRecoil();
+			ReceiveWeaponFire(Parent, MuzzleLocation);
+			OnWeaponFire.Broadcast(Parent, MuzzleLocation);
+		}
+
+		//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Bang!"));
+		NextFireTime = GetWorld()->GetTimeSeconds() + Firerate;
+		ReservoirCurrRoundCount--;
+		TotalAmmoCount--;
+
+		if (SoundPrimaryAttack)
+		{
+			UGameplayStatics::PlaySoundAtLocation(GetWorld(), SoundPrimaryAttack, GetActorLocation(), FRotator::ZeroRotator);
+		}
+	}
 }
 
 
--- Source/ProjectHoplomancer/Actors/Weapons/ShotgunPumpAction.h
+++ Source/ProjectHoplomancer/Actors/Weapons/ShotgunPumpAction.h
@@ -20,21 +20,20 @@ public:
 	// Sets default values for this actor's properties
 	AShotgunPumpAction();
 
-	virtual void PrimaryAttack(AActor* Parent, FVector MuzzleLocation, FRotator MuzzleRotation) override;
-
-	virtual void SecondaryAttack(AActor* Parent, FVector MuzzleLocation, FRotator MuzzleRotation) override;
-
 protected:
-
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Variable for the number of projectiles the weapon fires.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponStats)
 	int ProjectileCount;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void PrimaryAttack(AActor* Parent, FVector MuzzleLocation, FRotator MuzzleRotation) override;
+
+	virtual void SecondaryAttack(AActor* Parent, FVector MuzzleLocation, FRotator MuzzleRotation) override;
 
 };
